162cf1e3b466bae47ea9bb8723d85688e160a2fc
Vlad Los <losvlad521@gmail.com>
2024-10-26 13:58:25 +0300

    init

---
+++ ht-4.py
@@ -0,0 +1,12 @@
+some_list_1 = [1, 2, 3, 4, 5]
+some_list_2 = [101, 102, 103, 104, 105]
+print(some_list_1)
+print(some_list_2)
+print('=================')
+some_list_1.append(6)
+some_list_1.insert(0,0)
+print(some_list_1)
+some_list_2.append(106)
+some_list_2.insert(0,100)
+print(some_list_2)
+print('=================')
